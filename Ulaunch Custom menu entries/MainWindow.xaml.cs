3ff0e4cf0bbc4b91a5482514cf90795eed884c42
D3fau4 <raulwii2@gmail.com>
2019-10-29 16:36:16 +0100

    1.0.0

--- Ulaunch Custom menu entries/MainWindow.xaml.cs
+++ Ulaunch Custom menu entries/MainWindow.xaml.cs
@@ -30,20 +30,23 @@ namespace Ulaunch_Custom_menu_entries
         string iconfile = "";
 
         class jsonnro
-    {
-        public int type { get; set; }
+        {
+
+        public string type { get; set; }
+        public string nro_path { get; set; }
+        public string nro_argv { get; set; }
+        public string icon { get; set; }
         public string name { get; set; }
         public string author { get; set; }
-        public int version { get; set; }
-        public string nro_path { get; set; }
-        public string icon { get; set; }
+        public string version { get; set; }
+
         }
 
 
         public MainWindow()
         {
             InitializeComponent();
-       
+               
                 if (Directory.Exists(outfolder))
                 {
                     return;
@@ -60,6 +63,7 @@ namespace Ulaunch_Custom_menu_entries
         }
         private void IconButton_Click(object sender, RoutedEventArgs e)
         {
+            System.Windows.MessageBox.Show("It is recommended that the image has a size of 256x256");
             OpenFileDialog icon = new OpenFileDialog()
             {
                 Title = "Select icon for entrie",
@@ -68,25 +72,58 @@ namespace Ulaunch_Custom_menu_entries
                 InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
             };
             DialogResult res = icon.ShowDialog();
-            if (res is System.Windows.Forms.DialogResult.OK) iconfile = icon.FileName;
+            if (res is System.Windows.Forms.DialogResult.OK) iconfile = icon.FileName; else return;
+            NROicon.Source = new BitmapImage(new Uri(icon.FileName));
             //System.Windows.MessageBox.Show(iconfile);
         }
 
         private void MakeButton_Click(object sender, RoutedEventArgs e)
         {
             string idname = GetRandomString();
+            
+
+            if (String.IsNullOrEmpty(Namebox.Text))
+            {
+                System.Windows.MessageBox.Show("No application name was set.");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(AuthorBox.Text))
+            {
+                System.Windows.MessageBox.Show("No Author name was set.");
+                return;
+            }
+
+            if (NROpathBox.Text == "switch/")
+            {
+                System.Windows.MessageBox.Show("You havent chosen any path for the file");
+                return;
+            }
+            if (String.IsNullOrEmpty(iconfile))
+            {
+                System.Windows.MessageBox.Show("Icon file was not selected. This file is required to make the entrie.");
+                return;
+            }
+
             jsonnro p1 = new jsonnro 
-            { type = 2,
-                nro_path = NROpathBox.Text,
-                icon = outfolder + iconfolder,
+            { type = TypeBox.Text,
+                nro_path = "sdmc:/" + NROpathBox.Text,
+                nro_argv = NROargvBox.Text,
+                icon = "sdmc:/ulaunch/icons/" + idname + ".png",
                 name = Namebox.Text,
                 author = AuthorBox.Text,
+                version = VersionBox.Text
             };
             string outputJSON = JsonConvert.SerializeObject(p1);
             File.WriteAllText(idname +".json", outputJSON);
             File.Copy(iconfile, outfolder + iconfolder + idname + ".png");
             File.Move(idname + ".json", outfolder + jsonfolder + idname + ".json");
-            //System.Windows.MessageBox.Show(idname);
+            System.Windows.MessageBox.Show(Namebox.Text + " It has been created perfectly");
+            Namebox.Text = null;
+            AuthorBox.Text = null;
+            NROpathBox.Text = "switch/";
+            NROargvBox.Text = null;
+            VersionBox.Text = "1.0.0";
         }
     }
 }
